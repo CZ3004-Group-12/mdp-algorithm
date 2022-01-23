d1cde0f963f55b3a9f15cbfca28f850f8c22250f
yokemin <woonyokemin@gmail.com>
2022-01-24 03:04:16 +0800

    Add in start button

--- interface/buttons.py
+++ interface/buttons.py
@@ -17,7 +17,7 @@ def draw_button(surface, color, length, height, x, y, width):
 
 
 def write_text(surface, text, text_color, length, height, x, y):
-    font_size = int(length // len(text) * 2)
+    font_size = 16
     my_font = pygame.font.SysFont("Calibri", font_size)
     my_text = my_font.render(text, 1, text_color)
     surface.blit(my_text, ((x + length / 2) - my_text.get_width() / 2, (y + height / 2) - my_text.get_height() / 2))
--- interface/panel.py
+++ interface/panel.py
@@ -8,7 +8,10 @@ class Panel(object):
         # Buttons
         self.buttons = []
 
-        connect_button = Button(surface, colours.LIGHT_BLUE, 650, 120, 100, 25, "Connect to RPI", colours.BLACK, "CONNECT")
+        start_button = Button(surface, colours.GREEN, 650, 500, 100, 25, "START", colours.BLACK, "START")
+        self.buttons.append(start_button)
+
+        connect_button = Button(surface, colours.LIGHT_BLUE, 650, 120, 110, 25, "Connect to RPI", colours.BLACK, "CONNECT")
         self.buttons.append(connect_button)
         disconnect_button = Button(surface, colours.LIGHT_BLUE, 650, 150, 150, 25, "Disconnect from RPI", colours.BLACK, "DISCONNECT")
         self.buttons.append(disconnect_button)
